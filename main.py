2767f372c57693c7b5923b463aca067d294f7b5a
Sesilu00 <zS19014465@estudiantes.uv.mx>
2023-10-08 18:20:30 -0600

    Se aplicó el método "sobrante estocástico" para el algoritmo de selección de padres, en el futuro se buscarán maneras de optimizar el código.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 # Se utiliza representación por permutación, tomando el índice como fila y el valor como columna
 ini_subjects = [random.sample(range(8), 8) for i in range(0, 20)]
@@ -12,5 +13,14 @@ def ataques(subject):
             attacks = attacks + 1
     return int(attacks)
 
-print(ataques(ini_subjects[1]), ini_subjects[1])
 
+# Se aplicará el método de selección "Sobrante estocástico", utilizando la función de ataque
+# para calcular el fitness
+fitness_list = [ataques(ini_subjects[i]) for i in range(len(ini_subjects))]
+fitness_mean = sum(fitness_list)/len(fitness_list)
+val_esp = [fitness_list[i]/fitness_mean for i in range(len(fitness_list))]
+# Se crean dos listas, una tomando los tableros cuyo valor esperado tiene parte entera mayor
+# que 0 y otra tomando los tableros restantes con un volado.
+padres1 = [ini_subjects[i] for i in range(len(ini_subjects)) if math.trunc(val_esp[i]) > 0]
+padres2 = [ini_subjects[i] for i in range(len(ini_subjects)) if int(val_esp[i]) == 0
+           and random.random() > 0.5]
