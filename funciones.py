5bca44106d7a1efdaedeac3bd6a5ad214c97dc51
Sesilu00 <zS19014465@estudiantes.uv.mx>
2023-10-15 20:18:52 -0600

    Se creó la función para obtener los ciclos necesarios para la cruza.

--- funciones.py
+++ funciones.py
@@ -20,3 +20,24 @@ def mutacion(board):
         board[alelos[i + 1]] = gen1
         board[alelos[i]] = gen2
     return board
+
+
+def cruza1(sujeto_1, sujeto_2):
+    alelos_1 = {i: sujeto_1[i] for i in range(len(sujeto_1))}  # Diccionario de los alelos del primer sujeto
+    alelos_2 = {i: sujeto_2[i] for i in range(len(sujeto_2))}  # Diccionario de los alelos del segundo sujeto
+    ciclos = []  # lista de ciclos vacía
+    while alelos_1:
+        alelo = next(iter(alelos_1))  # Toma la primera llave disponible en alelos 1
+        gen = alelos_2[alelo]  # Busca ese alelo en alelos2 y da ese gen
+        ciclo = []  # El ciclo individual se crea vacío
+        while True:
+            ciclo.append(alelo)  # se agrega el alelo al ciclo
+            del alelos_1[alelo]  # se borra el alelo del diccionario
+            gen = alelos_2[alelo]  # se cambia el valor del gen al valor del alelo actual
+            alelo = gen  # se guarda el valor del gen en el alelo
+            if gen in alelos_1.values():  # se verifica que el gen esté en los disponibles
+                alelo = list(alelos_1.keys())[list(alelos_1.values()).index(alelo)]
+            else:
+                break
+        ciclos.append(ciclo)
+    return ciclos
