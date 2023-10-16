59f15105f1c6c4f7446c4f09dd701ad2a6065c07
Sesilu00 <zS19014465@estudiantes.uv.mx>
2023-10-15 21:39:49 -0600

    Se utilizaron las funciones de cruza para crear a los hijos a partir de los padres generados aleatoriamente en el main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import random
 import math
-from funciones import ataques, mutacion
+from funciones import ataques, mutacion, cruza1, cruza2
+#random.seed(99)
 # Se utiliza representación por permutación, tomando el índice como fila y el valor como columna
-ini_subjects = [random.sample(range(8), 8) for i in range(0, 20)]
+ini_subjects = [random.sample(range(8), 8) for i in range(0, 50)]
 
 # Se aplicará el método de selección "Sobrante estocástico", utilizando la función de ataque
 # para calcular el fitness
@@ -14,4 +15,27 @@ val_esp = [fitness_list[i]/fitness_mean for i in range(len(fitness_list))]
 padres1 = [ini_subjects[i] for i in range(len(ini_subjects)) if math.trunc(val_esp[i]) > 0]
 padres2 = [ini_subjects[i] for i in range(len(ini_subjects)) if int(val_esp[i]) == 0
            and random.random() > 0.5]
-print(val_esp)
+
+# Se crea una sola lista para los padres 1 y padres 2, seleccionándolos de forma aleatoria
+padres = padres1 + padres2
+hijos = []
+indices_padres = list(range(len(padres)))
+
+# Se utiliza un ciclo while para el caso cuando el número de padres sea impar
+while indices_padres:
+    selected_padres = random.sample(indices_padres, 2)
+    indices_padres.remove(selected_padres[0])
+    indices_padres.remove(selected_padres[1])
+
+    papa_1 = padres[selected_padres[0]]
+    papa_2 = padres[selected_padres[1]]
+
+    ciclos = cruza1(papa_1, papa_2)
+    hijo1 = cruza2(ciclos, papa_1, papa_2)
+    hijo2 = cruza2(ciclos, papa_2, papa_1)
+    hijos.append(hijo1)
+    hijos.append(hijo2)
+
+    if len(indices_padres) == 1:
+        break
+
